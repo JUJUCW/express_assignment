ca68f66279221c0f5d72fe95032022298954eae2
JUJUCW <juchung424@gmail.com>
2023-03-05 01:14:39 +0800

    FEAT/express_assignment

---
+++ app.js
@@ -0,0 +1,38 @@
+// app.js
+// require packages used in the project
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// require handlebars in the project
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// setting static files
+app.use(express.static('public'))
+
+// routes setting
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/portfolio', (req, res) => {
+    res.render('portfolio');
+});
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+// app.get('/:contact', (req, res) => {
+//     res.send(`<h1>${req.params.contact} is a popular language</h1>`);
+// });
+
+
+app.listen(port, () => {
+    console.log(`nodemon is running on http://localhost:${port}`);
+});
